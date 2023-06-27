d25994b64876c7e82792581b5f4af0f34a47825b
Tam L <tamlam@wearetopgroup.com>
2023-06-27 18:06:41 +0700

    test: husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
 	"*.{js,jsx,ts,tsx}": ["eslint --fix", "eslint"],
 	"**/*.ts?(x)": () => "npm run check-types",
-	"*": () => "npm run check-secret",
-	"*": () => "npm run format",
 };
