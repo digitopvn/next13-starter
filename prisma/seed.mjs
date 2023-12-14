89338e3d5ff8568a90793e8522dfd0e7bbd7076d
Tam L <tamlam@wearetopgroup.com>
2023-12-15 06:34:34 +0700

    fix: seed

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -1,5 +1,5 @@
+// @ts-nocheck
 import { PrismaClient } from "@prisma/client";
-import { createHmac } from "crypto";
 
 import Axios from "axios";
 
@@ -47,17 +47,13 @@ async function main() {
 	const findOrCreateUser = async (providerAccountId) => {
 		let existingUser = await prisma.user.findUnique({ where: { providerAccountId } });
 
-		const generateUUID = (await import("diginext-utils/dist/string/generateUUID.js")).generateUUID;
-
 		if (!existingUser) {
 			// Create default User_1 with Admin role
 			existingUser = await prisma.user.create({
 				data: {
 					name: "Admin",
-					providerAccountId: `Credentials-haohaotet2024-webapp-${generateUUID()}`, // you need to provide this value
-					password: createHmac("sha256", process.env.HMAC_KEY || "")
-						.update("Top@123#")
-						.digest("hex"),
+					providerAccountId, // you need to provide this value
+					password: "80e29ca4b22fbae54fdd0a77e31487f2e8cc7b250e89e75c0c62eebf8a2f0146",
 					email: "admin@digitop.vn",
 					userRoles: {
 						create: {
@@ -71,42 +67,49 @@ async function main() {
 				},
 			});
 
-			// //create token api
-			// const result = await Axios({
-			// 	url: `${process.env.NEXT_PUBLIC_API_UPLOAD_STORAGE}/v3/auth/create-token`,
-			// 	method: "POST",
-			// 	data: JSON.stringify({
-			// 		user: {
-			// 			id: existingUser.id,
-			// 			name: existingUser.name,
-			// 			email: existingUser.email || existingUser.emailOriginal,
-			// 			providerAccountId: existingUser.providerAccountId,
-			// 		},
-			// 	}),
-			// 	headers: {
-			// 		"Content-Type": "application/json",
-			// 	},
-			// });
-			// const { data } = result;
-			// if (data.data?.token?.key) {
-			// 	const updatedUser = await prisma.user.update({
-			// 		where: { id: existingUser.id },
-			// 		data: {
-			// 			token: {
-			// 				create: {
-			// 					key: data.data?.token?.key,
-			// 				},
-			// 			},
-			// 		},
-			// 		select: { token: true },
-			// 	});
+			//
+			//create token api
+			const result = await Axios({
+				url: `${process.env.NEXT_PUBLIC_API_UPLOAD_STORAGE}/v3/auth/create-token`,
+				method: "POST",
+				data: JSON.stringify({
+					user: {
+						id: existingUser.id,
+						name: existingUser.name,
+						email: existingUser.email || existingUser.emailOriginal,
+						providerAccountId: existingUser.providerAccountId,
+					},
+				}),
+				headers: {
+					"Content-Type": "application/json",
+				},
+
+				// contentType: "json",
+			});
+			// console.log("result :>> ", result);
+			// if (result.status) {
+			const { data } = result;
+			if (data.data?.token?.key) {
+				const updatedUser = await prisma.user.update({
+					where: { id: existingUser.id },
+					data: {
+						token: {
+							create: {
+								key: data.data?.token?.key,
+							},
+						},
+					},
+					select: { token: true },
+				});
+			}
+			// if (!data) return;
 			// }
 		}
 
 		return existingUser;
 	};
 
-	const admin = await findOrCreateUser("some_unique_value_here");
+	const admin = await findOrCreateUser(`Credentials-diginext13-admin`);
 
 	console.log("Seed done");
 }
